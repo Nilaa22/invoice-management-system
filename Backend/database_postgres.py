5d06e61817595de4ca53bcc04372b9d63c614606
Nilaa22 <kirubakalyani2526@gmail.com>
2026-07-21 17:00:39 -0400

    Configure database for Render

--- Backend/database_postgres.py
+++ Backend/database_postgres.py
@@ -1,13 +1,22 @@
+# import psycopg2
+
+# def pg_connection():
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         database="invoice_management",
+#         user="postgres",
+#         password="Bourbon005",
+#         port="5432"
+#     )
+
+#     return conn
+import os
 import psycopg2
 
 def pg_connection():
+    database_url = os.environ.get("DATABASE_URL")
 
-    conn = psycopg2.connect(
-        host="localhost",
-        database="invoice_management",
-        user="postgres",
-        password="Bourbon005",
-        port="5432"
-    )
+    conn = psycopg2.connect(database_url)
 
     return conn
